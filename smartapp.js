6352572425f07ac561c6c0c9e1fc1588657992aa
MightyPotatoe <tomasz.szymaniak93@hotmail.com>
2022-10-22 21:08:09 +0100

    Initial commit

--- smartapp.js
+++ smartapp.js
@@ -1,172 +1,90 @@
 const SmartApp = require('@smartthings/smartapp');
 const colors = require('./constants/colors');
 
-async function randomColorSwitch(context) {
-  // Return a random color from the colors object.
-  const randomColor = function (colors) {
-    const keys = Object.keys(colors);
-    return colors[keys[ keys.length * Math.random() << 0]];
-  };
-
-  // Get a random color.
-  const color = randomColor(colors);
-
-  // Send command to the SmartThings API.
-  await context.api.devices.sendCommands(context.config.lightSwitch, [
-    {
-      capability: 'switch',
-      command: 'on'
-    },
-    {
-      capability: 'switchLevel',
-      command: 'setLevel',
-      arguments: [100] // TODO: can we use the rate to fade between changes?
-    },
-    {
-      capability: 'colorControl',
-      command: 'setColor',
-      arguments: [color]
-    }
-  ]);
-}
-
-/* Define the SmartApp */
+//------------------------------Define the SmartApp ------------------------------
 module.exports = new SmartApp()
-  .configureI18n({updateFiles: true}) // Enable translations and update translation file when new items are added.
-  .enableEventLogging(2) // Logging for testing.
-  .page('mainPage', (context, page, configData) => {
-    // Define SmartApp page sections.
-    //Select Button
-    page.section('button', section => {
-    			section
-    				.deviceSetting('button')
-    				.capabilities(['button'])
-    				.required(true)
-    		})
+    .configureI18n({updateFiles: true}) // Enable translations and update translation file when new items are added.
+    .enableEventLogging(2) // Logging for testing.
+    .page('mainPage', (context, page, configData) => {
+        //---Connecting devices------------------------------------------
 
-     page.section('ledStrip', section => {
-        			section
-        				.deviceSetting('ledStrip')
-        				.capabilities(['colorControl'])
-        				.permissions('rx')
-        				.required(true)
-        		})
+        page.section('PcLedStrip', section => {
+            section
+                .deviceSetting('PcLedStrip')
+                .capabilities(['colorControl', 'switch'])
+                .permissions('rx')
+                .required(true)
+        });
 
-    // These are the sections for user input.
-    page.section('checkDuration', section => {
-      section
-        .enumSetting('lightCheckDuration')
-        .options([
-          { id: '1', name: '1 Minute' },
-          { id: '2', name: '2 Minutes' },
-          { id: '5', name: '5 Minutes' }
-        ])
-        .defaultValue('1');
-    });
-    page.section('lightSwitch', section => {
-      section
-        .deviceSetting('lightSwitch')
-        .capabilities(['switch'])
-        .permissions('rx')
-        .required(true);
-    });
-  })
-  .updated(async (context, updateData) => {
-    // Updated defines what code to run when the SmartApp is installed or the settings are updated by the user.
+        page.section('PcRgbLight', section => {
+            section
+                .deviceSetting('PcRgbLight')
+                .capabilities(['colorControl', 'switch'])
+                .permissions('rx')
+                .required(true);
+        });
+    })
 
-     // Clear any existing configuration.
-    await context.api.schedules.delete()
-    await context.api.subscriptions.delete()
+    .updated(async (context, updateData) => {
+        // Updated defines what code to run when the SmartApp is installed or the settings are updated by the user.
 
-    //Subscribe to events
-    await context.api.subscriptions.subscribeToDevices(context.config.button, 'button', 'button', 'pcButtonHandler')
-    await context.api.subscriptions.subscribeToDevices(context.config.ledStrip, 'colorControl', 'hue', 'pcLedStripHueHandler')
+        // Clear any existing configuration.
+        await context.api.schedules.delete()
+        await context.api.subscriptions.delete()
 
+        //Subscribe to events
+        await context.api.subscriptions.subscribeToDevices(context.config.PcLedStrip, 'switch', 'switch', 'pcLedStripSwitchHandler')
+        await context.api.subscriptions.subscribeToDevices(context.config.PcLedStrip, 'colorControl', 'hue', 'pcLedStripHueHandler')
 
-    // Set initial switch toggle.
-    await randomColorSwitch(context);
+    })
 
-    // Schedule future toggle checks.
-    await context.api.schedules.schedule(
-      'lightScheduleHandler', 
-      `0/${context.configStringValue('lightCheckDuration')} * * * ? *`, 
-      'UTC'
-    );
-  })
-  .subscribedEventHandler('pcButtonHandler', async (context, event) => {
-    if(event.value === 'pushed'){
-        context.api.devices.sendCommands(context.config.lightSwitch, 'switch', 'on')
-        turnOn(context)
-    }
-    else if(event.value === 'held'){
-        context.api.devices.sendCommands(context.config.lightSwitch, 'switch', 'off')
-        context.api.devices.sendCommands(context.config.ledStrip, 'switch', 'off')
-    }
-  })
+    .subscribedEventHandler('pcLedStripHueHandler', async (context, event) => {
+        stripStatus = await context.api.devices.getCapabilityStatus(context.config.PcLedStrip[0].deviceConfig.deviceId, 'main', 'colorControl')
+        satValue = stripStatus.saturation.value
+        hueValue = stripStatus.hue.value
+        if (satValue !== 0) {
+            //Setting hue to PC light
+            changeColor(context, context.config.PcRgbLight, satValue, hueValue)
+        } else{
+            //Setting color temp
+            changeColorTemp(context, context.config.PcRgbLight, satValue, hueValue)
+        }
+    })
 
-   .subscribedEventHandler('pcLedStripHueHandler', async (context, event) => {
-       context.api.devices.sendCommands(context.config.lightSwitch, 'switch', 'on')
-       console.log('*************************************************************************************************')
-       console.log('*************************************************************************************************')
-//       objjson = JSON.parse(context.config.ledStrip)
-       stripStatus = await context.api.devices.getCapabilityStatus(context.config.ledStrip[0].deviceConfig.deviceId, 'main', 'colorControl')
-       satValue = stripStatus.saturation.value
-       hueValue = stripStatus.hue.value
-       changeColor(context, satValue, hueValue)
+    .subscribedEventHandler('pcLedStripSwitchHandler', async (context, event) => {
+        if(event.value === 'on'){
+            context.api.devices.sendCommands(context.config.PcRgbLight, 'switch', 'on')
+        }
+        else {
+            context.api.devices.sendCommands(context.config.PcRgbLight, 'switch', 'off')
+        }
+    })
 
-//       object = context.config.ledStrip
-//       console.log(object)
-       console.log('*************************************************************************************************')
-       console.log('*************************************************************************************************')
-
-   })
-
-  .scheduledEventHandler('lightScheduleHandler', async (context, event) => {
-    // Every duration chosen by user, toggle the light switch.
-//    randomColorSwitch(context);
-  });
-
-  async function turnOn(context){
-
-  const randomColor = function (colors) {
-      const keys = Object.keys(colors);
-      return colors[keys[ keys.length * Math.random() << 0]];
-    };
-
-    // Get a random color.
-    const color = randomColor(colors);
-
-  // Send command to the SmartThings API.
-    await context.api.devices.sendCommands(context.config.ledStrip, [
-      {
+async function changeColor(context, device, saturation, hue) {
+    // Send command to the SmartThings API.
+    await context.api.devices.sendCommands(device, [
+        {
             capability: 'switch',
             command: 'on'
-          },
-          {
+        },
+        {
             capability: 'colorControl',
             command: 'setColor',
             arguments: [{
-                hue: 10,
-                saturation: 50
+                hue: hue,
+                saturation: saturation
             }]
-          }
-        ]);
-  }
+        }
+    ]);
+}
 
-  async function changeColor(context, saturation, hue){
+async function changeColorTemp(context, device) {
     // Send command to the SmartThings API.
-        await context.api.devices.sendCommands(context.config.lightSwitch, [
-          {
-                capability: 'switch',
-                command: 'on'
-              },
-              {
-                capability: 'colorControl',
-                command: 'setColor',
-                arguments: [{
-                    hue: hue,
-                    saturation: saturation
-                }]
-              }
-            ]);
-  }
+    await context.api.devices.sendCommands(device, [
+        {
+            capability: 'colorTemperature',
+            command: 'setColorTemperature',
+            arguments: [6500]
+        }
+    ]);
+}
